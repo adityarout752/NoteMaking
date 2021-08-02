0ac72eacc394215166b64f0abdb9543ffe177023
@Aditya rout <adityaroutgood@gmail.com>
2021-08-02 12:37:38 +0530

    Navigation graph setup and layout part done

---
+++ app/src/main/java/com/example/notesapp/ui/fragments/NewNoteFragment.kt
@@ -0,0 +1,8 @@
+package com.example.notesapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.notesapp.R
+
+
+class NewNoteFragment :Fragment(R.layout.fragment_new_note){
+}
---
+++ app/src/main/java/com/example/notesapp/ui/fragments/UpdateNoteFragment.kt
@@ -0,0 +1,9 @@
+package com.example.notesapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.notesapp.R
+
+
+class UpdateNoteFragment:Fragment(R.layout.fragment_update_note) {
+
+}
